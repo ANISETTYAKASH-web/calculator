f26515f329a917218e259d5adbdcf78faa102c5d
ANISETTY AKASH <akashanisetty7@gmail.com>
2024-10-14 10:52:24 +0530

    basic functioning of calculator done still lot of edge cases to solve and clear button doesn't work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const calculator = document.querySelector('.calculator');
 //console.log(calculator);
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 keys.addEventListener('click',e=>{
   if(e.target.matches('button'))
 {
@@ -20,22 +21,42 @@ if(!action)
 if(action=='decimal') {console.log('decimal');}
 if(action=='clear') {console.log('clear');}
 if(action=='equal') {console.log('equal');}
-
+const keyContent = key.textContent;
+const displayedNum = display.textContent;
+if(!action)
+  {
+    if(displayedNum==='0') display.textContent=keyContent;
+    else display.textContent = displayedNum+keyContent;
+  }
+  if(action==='decimal') display.textContent=displayedNum+'.';
+  if(action==='add'||action==='sub'||action==='mul'||action==='divide')
+    {
+      calculator.dataset.firstValue=displayedNum;
+      calculator.dataset.operator=action;
+      calculator.dataset.previousKeyType='operator';
+    }
+    const previousKeyType = calculator.dataset.previousKeyType;
+    if(!action)
+    {
+      if(displayedNum==='0'||previousKeyType==='operator') display.textContent = keyContent
+      else display.textContent = displayedNum+keyContent;
+    }
+    if(action==='equal')
+    {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum
+      display.textContent = calculate(firstValue,operator,secondValue);
+    }
+    
 }});
-const display = document.querySelector('.calculator__display');
-keys.addEventListener('click',e=>{
-   if(e.target.matches('button')){
-      const key = e.target;
-      console.log(key + "akshj");
-      const action = key.dataset.action;
-      const keyContent = key.textContent;
-      const displayedNum = display.textContent;
-      if(!action)
-      {
-        if(displayedNum==='0') display.textContent=keyContent;
-        else display.textContent = displayedNum+keyContent;
-      }
-      if(action==='decimal') display.textContent=displayedNum+'.';
-   }
-})
+function calculate(num1,op,num2)
+{
+  let result= '';
+  if(op==='add') result= parseFloat(num1)+parseFloat(num2);
+  if(op==='sub') result = parseFloat(num1)-parseFloat(num2);
+  if(op==='mul') result=parseFloat(num1)*parseFloat(num2);
+  if(op==='divide') result=parseFloat(num1)/parseFloat(num2);
+  return result;
+}
 
